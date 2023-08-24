9f15456ea6d0c769fc4e4c76ab4a520f7e1f6f2f
afr117 <6666@holbertonstudents.com>
2023-08-24 00:22:21 -0700

    1..100

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,6 +13,7 @@ size_t max(size_t a, size_t b)
 	return (a > b ? a : b);
 }
 
+
 /**
  * binary_tree_height - Measures the height of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the height.
@@ -22,7 +23,7 @@ size_t max(size_t a, size_t b)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0);
+	return (0);
 
 	return (1 + max(binary_tree_height(tree->left), binary_tree_height(tree->right)));
 }
@@ -38,18 +39,17 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
+	return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height == right_height)
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (1);
-		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-	}
-
-	return (0);}
+{
+	if (tree->left == NULL && tree->right == NULL)
+	return (1);
+	return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 }
 
+	return (0);
+}
