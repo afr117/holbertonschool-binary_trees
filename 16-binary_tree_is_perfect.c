9f0b13d8235bcd5bd0e6fc648f032342331fe909
afr117 <6666@holbertonstudents.com>
2023-08-24 00:39:30 -0700

    1..100

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,7 +25,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 	return (0);
 
-	return (1 + max(binary_tree_height(tree->left), binary_tree_height(tree->right)));
+	return (1 + max(binary_tree_height(tree->left),
+				binary_tree_height(tree->right)));
 }
 
 /**
